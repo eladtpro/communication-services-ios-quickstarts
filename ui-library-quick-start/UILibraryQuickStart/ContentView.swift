c0d6bb5cf715c02dd324c62941cb6a593fd3270b
eladtpro <eladt.pro@gmail.com>
2023-01-02 15:22:57 +0200

    working version - before cleanup

---
+++ ui-library-quick-start/UILibraryQuickStart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  FormView.swift
+//  UILibraryQuickStart
+//
+//  Created by Elad Tal on 18/12/2022.
+//
+
+import SwiftUI
+
+struct FormView: View {
+    var body: some View {
+        Form{
+            Section(header: "Azure Communication Services")){
+                Text()
+            }
+        }
+    }
+}
+
+struct FormView_Previews: PreviewProvider {
+    static var previews: some View {
+        FormView()
+    }
+}
